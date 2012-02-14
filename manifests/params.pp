10c4797ca52a86ca077c133a8db88b9601836aee
Steffen Zieger <me@saz.sh>
2012-02-14 14:00:43 +0100

    style-guide fixes, default for unsupported platform, fully-qualified variable

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,16 @@
 class ccollect::params {
-    case $ccollect::backup_dir {
-        '': { $backup_dir = '/var/lib/backups/' }
-        default: { $backup_dir = $ccollect::backup_dir }
-    }
+  case $ccollect::backup_dir {
+    '': { $backup_dir = '/var/lib/backups/' }
+    default: { $backup_dir = $ccollect::backup_dir }
+  }
 
-    case $operatingsystem {
-        /(Ubuntu|Debian)/: {
-            $package_name = 'ccollect'
-            $config_dir = '/etc/ccollect/'
-        }
+  case $::operatingsystem {
+    ubuntu, debian: {
+      $package_name = 'ccollect'
+      $config_dir = '/etc/ccollect/'
     }
+    default: {
+      fail("Unsupported platform: ${::operatingsystem}")
+    }
+  }
 }
