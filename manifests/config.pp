fc701a92d062751a352ed7205932a578d19b3c9a
Steffen Zieger <me@saz.sh>
2014-01-29 10:37:08 +0100

    manage default backup location

--- manifests/config.pp
+++ manifests/config.pp
@@ -21,6 +21,15 @@ class ccollect::config {
     require => Class['ccollect::install'],
   }
 
+  file { "${ccollect::params::config_dir}sources/destination":
+    ensure  => file,
+    owner   => 0,
+    group   => 0,
+    mode    => '0644',
+    content => "${ccollect::backup_dir}\n",
+    require => File["${ccollect::params::config_dir}sources"],
+  }
+
   file { $ccollect::params::backup_dir:
     ensure => directory,
     owner  => root,
